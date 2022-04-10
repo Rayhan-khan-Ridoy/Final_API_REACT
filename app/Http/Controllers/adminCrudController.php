42a79c590b8a5230fb6f60a00c91f97553b28b19
Rayhan-khan-Ridoy <78033774+Rayhan-khan-Ridoy@users.noreply.github.com>
2022-04-10 15:53:25 +0600

    some updations

--- app/Http/Controllers/adminCrudController.php
+++ app/Http/Controllers/adminCrudController.php
@@ -8,20 +8,33 @@ use App\Models\Customer;
 use App\Models\Product;
 use App\Models\Officer;
 use App\Models\Cperformance;
+use App\Models\Branche;
 
 class adminCrudController extends Controller
 {
-  public function viewAllAdmin(){
-                    $admins = Adminregistration::select('id','name','username','email','gender','pro_pic')->get();
-                    //return view('viewAllAdmin')->with('admins',$admins);
-                    return response()->json($admins);
-    }
+  public function viewAllAdmin()
+  {
+          $admins = Adminregistration::select('id', 'name', 'username', 'email', 'gender', 'pro_pic')->get();
+          //return view('viewAllAdmin')->with('admins',$admins);
+          return response()->json($admins);
+  }
+
+  public function viewInduvidualAdmin(Request $req)
+  {
+          $admins = Adminregistration::where('id', $req->id)->first();
+
+          return response()->json([
+            "msg" => "this is individual Admin info!",
+            "admin" => $admins
+          ]);
+  }
 
 
 
 
-  public function adminEdit(Request $req){
-            /*$this->validate($req,
+  public function adminEdit(Request $req)
+  {
+    /*$this->validate($req,
                 [
                     'name'=>'required',
                     'username'=>'required|regex:/^[A-Z a-z . -]+$/|min:5|max:20|unique:adminregistrations,username',//
@@ -39,26 +52,33 @@ class adminCrudController extends Controller
 
 
 
-                $admin = Adminregistration::where('id',$req->id)->first();
+          $admin = Adminregistration::where('id', $req->id)->first();
 
-                  $admin->name=$req->name;
-                  $admin->username=$req->username;
-                  $admin->email=$req->email;
-                  $admin->gender=$req->gender;
-             
-                  $admin->save();
-                  session()->flash('msg2','Updated successfully!');
-                  return response()->json($admin);
-        }
+          $admin->name = $req->name;
+          $admin->username = $req->username;
+          $admin->email = $req->email;
+          $admin->gender = $req->gender;
+
+          $admin->save();
+          session()->flash('msg2', 'Updated successfully!');
+          return response()->json([
+            "msg" => "Admin Edited Succesfully !",
+            "admin" => $admin
+          ]);
+  }
 
 
-  public function adminDelete(Request $req){
+  public function adminDelete(Request $req)
+  {
 
-                  $admin = Adminregistration::where('id',($req->id))->delete();
-                  session()->flash('msg3','Admin deleted successfully!');
-                  return response()->json($admin);                }
+          $admin = Adminregistration::where('id', ($req->id))->delete();
+          session()->flash('msg3', 'Admin deleted successfully!');
+          return response()->json([
+            "msg" => "Admin Deleted Succesfully !"
+          ]);
+  }
 
-/*
+  /*
 //--------------start  searching
 public function searchAdmin(Request $req){
 
@@ -126,45 +146,47 @@ public function searchEmployee(Request $req){
 //--------------end  searching
 */
 
-//--------------start user or customer + employee + company performane activities
-public function addPerformance(Request $req){
- /* $this->validate($req,
-  [
-      'Year'=>'required|unique:cperformances,Year',
-      'Sales'=>'required',//
-      'Expenses'=>'required'
-    ],
-  [
-      'Year.required'=>'Please provide year',
-      'Sales.required'=>'Please provide Sales',
-      'Expenses.required'=>'Please provide Expenses'
+  //--------------start user or customer + employee + company performane activities
+  public function addPerformance(Request $req)
+  {
+          /* $this->validate($req,
+        [
+            'Year'=>'required|unique:cperformances,Year',
+            'Sales'=>'required',//
+            'Expenses'=>'required'
+          ],
+        [
+            'Year.required'=>'Please provide year',
+            'Sales.required'=>'Please provide Sales',
+            'Expenses.required'=>'Please provide Expenses'
 
 
-  ]
-);*/
+        ]
+      );*/
 
-        $perf = new Cperformance();
+          $perf = new Cperformance();
 
-        $perf->Year = $req->Year;
-        $perf->Sales = $req->Sales;
-        $perf->Expenses = $req->Expenses;
+          $perf->Year = $req->Year;
+          $perf->Sales = $req->Sales;
+          $perf->Expenses = $req->Expenses;
 
-        $perf -> save(); //runs query in db
+          $perf->save(); //runs query in db
 
-        session()->flash('msg4','Performances added!');
+          session()->flash('msg4', 'Performances added!');
 
-        //return back();
-        return response()->json(["msg"=>"Performance added Succesfully!",
-                                  "performance"=>$perf
-                                ]);
+          //return back();
+          return response()->json([
+            "msg" => "Performance added Succesfully!",
+            "performance" => $perf
+          ]);
+  }
 
-      }
 
-                            
 
-public function editPerformance(Request $req){
+  public function editPerformance(Request $req)
+  {
 
-  /*                  $this->validate($req,
+    /*                  $this->validate($req,
                         [
                             'Year'=>'required|unique:cperformances,Year',
                             'Sales'=>'required',//
@@ -179,95 +201,183 @@ public function editPerformance(Request $req){
                         ]
                     );
 */
-                              
-                              $perf = Cperformance::where('id',$req->id)->first();
-                              $perf->Year = $req->Year;
-                              $perf->Sales = $req->Sales;
-                              $perf->Expenses = $req->Expenses;
 
-                              $perf -> save(); //runs query in db
+          $perf = Cperformance::where('id', $req->id)->first();
+          $perf->Year = $req->Year;
+          $perf->Sales = $req->Sales;
+          $perf->Expenses = $req->Expenses;
 
-                              session()->flash('msg4','Performances added!');
+          $perf->save(); //runs query in db
 
-                              return response()->json([
-                                                      "msg"=>'Performances Edited!',
-                                                      "performance"=>$perf
-                                                    ]);
+          session()->flash('msg4', 'Performances added!');
 
-                            }
-
-
-  public function viewAllPerformance(){
-                                    $perf = Cperformance::select('id','Year','Sales','Expenses')->get();
-
-                                    //  dd($userCount);
-                                    return response()->json($perf);
-                                    }
-
-public function performaneDelete(Request $req){
-
-                $perf = Cperformance::where('id',($req->id))->delete();
-                session()->flash('msg3','Performance deleted successfully!');
-                //return redirect ()->route ( 'viewAllPerformance' );
-                return response()->json($perf);
-              }
+          return response()->json([
+            "msg" => 'Performances Edited!',
+            "performance" => $perf
+          ]);
+  }
 
 
+  public function viewAllPerformance()
+  {
+          $perf = Cperformance::select('id', 'Year', 'Sales', 'Expenses')->get();
 
+          //  dd($userCount);
+          return response()->json([
+            "msg" => "All Company performances are: !",
+            "performances" => $perf
+          ]);
+  }
 
+  public function viewInduvidualPerformance(Request $req)
+  {
+          $perf = Cperformance::where('id', $req->id)->first();
 
-public function viewAllEmployee(){
-                                    $employees = Officer::select('id','name','email','salary','address')->get();
+          return response()->json([
+            "msg" => "This is individual Performance!",
+            "performance" => $perf
+          ]);
+  }
 
-                                    //dd($employees);
-                                    //return view('viewAllEmployee')->with('employees',$employees);
-                                    return response()->json($employees);
-                                  }
-public function employeeDelete(Request $req){
+  public function performaneDelete(Request $req)
+  {
 
-                                    $emp = Officer::where('id',($req->id))->delete();
-                                   // session()->flash('msg3','Employee deleted successfully!');
-                                    //return redirect ()->route ( 'viewAllEmployee' );
-                                    return response()->json(["msg"=>"Employee deleted Succesfully!"]);
-                                  }
-public function viewAllUser(){
-                                  $users = Customer::select('id','username','email','created_at')->get();
-
-                                  //  dd($userCount);
-                                   // return view('viewAllUser')->with('users',$users);
-                                   return response()->json($users);
-                                  }
-
-
-public function userDelete(Request $req){
-
-                                    $admin = Customer::where('id',($req->id))->delete();
-                                    session()->flash('msg3','Customer deleted successfully!');
-                                    //return redirect ()->route ( 'viewAllUser' );
-                                    return response()->json([
-                                      "msg"=>'user deleted!'
-                                    ]);
-                                  }
-//--------------end activities
+          $perf = Cperformance::where('id', ($req->id))->delete();
+          session()->flash('msg3', 'Performance deleted successfully!');
+          //return redirect ()->route ( 'viewAllPerformance' );
+          return response()->json($perf);
+  }
 
 
 
-//--------------start product  activities
 
-  public function viewAllProducts(){
-                    $products = Product::select('id','pname','quantity','price','category')->get();
-                    //dd($products);
-                    //return view('viewAllProducts')->with('products',$products);
-                    return response()->json($products);
 
-                  }
-  public function productDelete(Request $req){
+  public function viewAllEmployee()
+  {
+    $employees = Officer::select('id', 'name', 'email', 'salary', 'address')->get();
 
-                    $products = Product::where('id',($req->id))->delete();
-                   // session()->flash('msg3','Product deleted successfully!');
-                    //return redirect ()->route ( 'viewAllProducts' );
-                    return response()->json(["msg"=>"Product deleted Succesfully!"]);
-                  }
+          //dd($employees);
+          //return view('viewAllEmployee')->with('employees',$employees);
+          return response()->json([
+            "msg" => "All Officers are: !",
+            "Officers" => $employees
+          ]);
+  }
 
-//--------------end product activities
+  public function viewInduvidualEmployee(Request $req)
+  {
+    $Officer = Officer::where('id', $req->id)->first();
+
+          return response()->json([
+            "msg" => "this is individual Officer!",
+            "Officer" => $Officer
+          ]);
+  }
+
+  public function employeeDelete(Request $req)
+  {
+
+          $emp = Officer::where('id', ($req->id))->delete();
+          return response()->json(["msg" => "Employee deleted Succesfully!"]);
+  }
+
+  //----------user activities-----------------
+  public function viewAllUser()
+  {
+          $users = Customer::select('id', 'username', 'email', 'created_at')->get();
+
+          //  dd($userCount);
+          // return view('viewAllUser')->with('users',$users);
+          return response()->json([
+            "msg" => "All customers are: !",
+            "customers" => $users
+          ]);
+  }
+  public function viewInduvidualUser(Request $req)
+  {
+          $users = Customer::where('id', $req->id)->first();
+
+          return response()->json([
+            "msg" => "this is individual user!",
+            "users" => $users
+          ]);
+  }
+
+  public function userDelete(Request $req)
+  {
+
+          $users = Customer::where('id', ($req->id))->delete();
+          session()->flash('msg3', 'Customer deleted successfully!');
+          //return redirect ()->route ( 'viewAllUser' );
+          return response()->json([
+            "msg" => 'user deleted!'
+          ]);
+  }
+  //--------------end activities
+
+
+
+  //--------------start product  activities
+
+  public function viewAllProducts()
+  {
+          $products = Product::select('id', 'pname', 'quantity', 'price', 'category')->get();
+          return response()->json($products);
+  }
+
+  public function viewInduvidualProduct(Request $req)
+  {
+          $product = Product::where('id', $req->id)->first();
+
+          return response()->json([
+            "msg" => "this is individual Product!",
+            "product" => $product
+          ]);
+  }
+
+  public function productDelete(Request $req)
+  {
+
+          $products = Product::where('id', ($req->id))->delete();
+          return response()->json(["msg" => "Product deleted Succesfully!"]);
+  }
+
+  //--------------end product activities
+
+
+  //--------------start branchs activities
+  public function viewAllBranche()
+  {
+    $branchs = Branche::select('id', 'bname', 'email')->get();
+
+          return response()->json([
+            "msg" => "All Branches are!",
+            "branch" => $branchs
+          ]);
+  }
+  public function viewInduvidualBranche(Request $req)
+  {
+    $branchs = Branche::where('id', $req->id)->first();
+
+          return response()->json([
+            "msg" => "this is individual Branch!",
+            "branch" => $branchs
+          ]);
+  }
+  public function editBranche(Request $req)
+  {
+          $branchs = Branche::where('id', $req->id)->first();
+
+          $branchs->bname = $req->bname;
+          $branchs->email = $req->email;
+          $branchs->pro_pic = $req->pro_pic;
+
+          $branchs->save(); //runs query in db
+          return response()->json([
+            "msg" => "Branch edited Succesfully!",
+            "branch" => $branchs
+          ]);
+  }
+
+  //--------------end branchs activities
 }
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\adminController;
 use App\Http\Controllers\adminCrudController;
 
 /*
@@ -19,6 +18,8 @@ use App\Http\Controllers\adminCrudController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//------start ridoy-----
 /*
 Route::get('/adminLogin',[adminController::class,'adminLogin'])->name('adminLogin');
 Route::post('/adminLoginSubmit',[adminController::class,'adminLoginSubmit'])->name('adminLoginSubmit');
@@ -33,6 +34,16 @@ Route::get('/viewAllAdmin',[adminCrudController::class,'viewAllAdmin'])->name('v
 Route::get('/viewAllUser',[adminCrudController::class,'viewAllUser'])->name('viewAllUser');//->middleware('authorized');
 Route::get('/viewAllProducts',[adminCrudController::class,'viewAllProducts'])->name('viewAllProducts');//->middleware('authorized');
 Route::get('/viewAllEmployee',[adminCrudController::class,'viewAllEmployee'])->name('viewAllEmployee');//->middleware('authorized');
+Route::get('/viewAllBranche',[adminCrudController::class,'viewAllBranche'])->name('viewAllBranche');//->middleware('authorized');
+
+Route::get('/viewInduvidualBranche/{id}',[adminCrudController::class,'viewInduvidualBranche'])->name('viewInduvidualBranche');//->middleware('authorized');
+Route::get('/viewInduvidualProduct/{id}',[adminCrudController::class,'viewInduvidualProduct'])->name('viewInduvidualProduct');
+Route::get('/viewInduvidualEmployee/{id}',[adminCrudController::class,'viewInduvidualEmployee'])->name('viewInduvidualEmployee');
+Route::get('/viewInduvidualUser/{id}',[adminCrudController::class,'viewInduvidualUser'])->name('viewInduvidualUser');
+Route::get('/viewInduvidualPerformance/{id}',[adminCrudController::class,'viewInduvidualPerformance'])->name('viewInduvidualPerformance');
+Route::get('/viewInduvidualAdmin/{id}',[adminCrudController::class,'viewInduvidualAdmin'])->name('viewInduvidualAdmin');
+
+Route::put('/editBranche/{id}',[adminCrudController::class,'editBranche'])->name('editBranche');
 
 Route::put('/adminEdit/{id}',[adminCrudController::class,'adminEdit'])->name('adminEdit');
 Route::get('/adminDelete/{id}',[adminCrudController::class,'adminDelete'])->name('adminDelete');
@@ -45,3 +56,6 @@ Route::post('/addPerformance',[adminCrudController::class,'addPerformance'])->na
 Route::put('/editPerformance/{id}',[adminCrudController::class,'editPerformance'])->name('editPerformance');
 Route::get('/viewAllPerformance',[adminCrudController::class,'viewAllPerformance'])->name('viewAllPerformance');
 Route::get('/performaneDelete/{id}',[adminCrudController::class,'performaneDelete'])->name('performaneDelete');
+
+
+//-----end ridoy------
